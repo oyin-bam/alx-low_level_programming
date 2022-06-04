b072951b86b224893037f9d030a16d18a45dbc54
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-04 11:19:36 -0700

    printing lower case then upper case letters using putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main- this function prints the lower then uppercase alphabets
+ *
+ * Return: this returns 0 on success
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
+
