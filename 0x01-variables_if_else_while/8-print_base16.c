58acfc60e4983fa71bf36968ff9288cd2f934c47
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-05 05:03:21 -0700

    printing all hexadecimal numbers in lowercase

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main- this prints all the hexadecimal numbers
+ *
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+}
