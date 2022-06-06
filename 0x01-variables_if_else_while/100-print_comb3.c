9eca4e3bd0b5263c0374a6e79dac9987171c856e
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-06 02:15:00 -0700

    printing possible different combinations of two digits using putchar

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+/**
+ * main- this program prints all possible different combinations of two digits
+ *
+ * Return: returns 0 on success
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			while (i >= j)
+			{
+				j++;
+			}
+			if (i == 9)
+			{
+				break;
+			}
+			else
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				if (i == 8 && j == 9)
+				{
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+}
