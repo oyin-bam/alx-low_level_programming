c062835c73aeb5a5d1bf5eeee4a8490b05eef0f3
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-07 14:04:30 -0700

    correction 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,16 +8,10 @@
 void print_alphabet_x10(void)
 {
 	int i;
-
+	
 	for (i = 0; i < 10; i++)
 	{
-		int j;
-
-		for (j = 97; j <= 122; j++)
-		{
-			_putchar(j);
-		}
-		_putchar('\n');
+		print_alphabet();
 	}
 	_putchar('\n');
 }
