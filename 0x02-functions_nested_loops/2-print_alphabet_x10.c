541ad4257268723126921dfb18c42c3bd41249aa
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-07 10:49:38 -0700

    custom printng 10 lines of the lower case alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main- prints the lower case alphabets on 10 lines
+ *
+ * return: returns nothing. void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		int j;
+
+		for (j = 97; j <= 122; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n')
+	}
+	_putchar('\n');
+}
