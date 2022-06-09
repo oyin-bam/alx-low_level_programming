14a9f17091a0db45d9602ac3efcc4049eaa2ea20
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-09 10:19:50 -0700

    capitalize fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,15 +16,15 @@ int main(void)
 	{
 		if ((i % 3 == 0) && (i % 5 != 0))
 		{
-			printf("fizz ");
+			printf("Fizz ");
 		}
 		else if ((i % 3 != 0) && (i % 5 == 0))
 		{
-			printf("buzz ");
+			printf("Buzz ");
 		}
 		else if ((i % 3 == 0) && (i % 5 == 0))
 		{
-			printf("fizzbuzz ");
+			printf("FizzBuzz ");
 		}
 		else
 		{
