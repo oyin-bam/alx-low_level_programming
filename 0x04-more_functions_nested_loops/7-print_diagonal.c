00a24b3f33b1ff102a1c6e517057324df65c7eb3
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-09 07:54:21 -0700

    diagonal line correction

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,17 +8,24 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-
-	if (n > 0)
+	if (n <= 0)
 	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+		int j;
+
 		for (i = 0; i < n; i++)
 		{
-			_putchar(92);
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('\n');
-	}
+}
