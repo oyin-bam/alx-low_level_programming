1f0e50e5ca1415eed7c2993ae89df089e487ecee
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-15 17:49:05 -0700

    handled all cases plus zero pointer in string comparator

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,28 +9,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
-	int j;
-
-	j = 0;
-	while (s1[j] != '\0')
+	while (*s1 == *s2)
 	{
-		j++;
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
 	}
-	for (i = 0; i < j; i++)
-	{
-		if (s1[i] == s2[i])
-		{
-			i++;
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (s1[i] - s2[i]);
-		}
-		else
-		{
-			return (s1[i] - s2[i]);
-		}
-	}
-	return (0);
+	return (*s1 - *s2);
 }
