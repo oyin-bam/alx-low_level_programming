2ce03a3f64a9f3bcd3dde1caa6487744a0e06a1a
oyin-bam <oyindaakinyemi@gmail.com>
2022-06-20 06:05:34 -0700

    changing the memory area value

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memset- this  fills the first n bytes of the memory area
+ * pointed to by s with the constant byte b
+ * @s: memory area being pointed to
+ * @b: byte to be placed in each byte position of s
+ * @n: number of bytes in s
+ *
+ * Return: this returns a pointer to s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char *l;
+	
+	l = &(*s);
+	for (i = 0; i < n; i++)
+	{
+		l[i] = b;
+	}
+	return (s);
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+this directory contains more exercises on pointers and arrays
+touches on pointers to pointers as well as multidimensional arrays
