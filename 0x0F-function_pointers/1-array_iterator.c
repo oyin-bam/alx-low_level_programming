59341b913299a1abb243240972747a643b22e0ce
oyin-bam <oyindaakinyemi@gmail.com>
2022-07-07 05:37:40 -0700

    array iterator using pointer to function

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator-  function that executes a function
+ * given as a parameter on each element of an array.
+ * @array:array to be iterated through
+ * @size: size of the array
+ * @action:function to be performed on each element of the array
+ *
+ * Return: returns nothing
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+	{
+		return;
+	}
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
