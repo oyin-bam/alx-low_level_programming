5e4342af8f38b573e061e3fe7dcd44da6c8e553b
oyin-bam <oyindaakinyemi@gmail.com>
2022-07-07 12:42:47 -0700

    betty style correction

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -4,15 +4,16 @@
 /**
  * sum_them_all- function that returns the sum of all its parameters
  * using variadic functions
- * @int: the number of parameters we want to be considered from the users entry
+ * @n: the number of parameters we want to be considered from the users entry
  *
  * Return: this returns the resulting sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	unsigned int i ;
+	unsigned int i;
 	int sum;
 	va_list ap;
+
 	va_start(ap, n);
 
 	sum = 0;
